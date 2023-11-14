bfb3c4973dcd082e89994a7c80643b79bfed0d60
XU BIAN <bxu@skillbase.fr>
2023-11-14 16:31:37 +0800

    UPDATE

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 
 from index import KaiStudio
 from index import KaiStudioCredentials
@@ -11,7 +12,7 @@ class Credentials(KaiStudioCredentials):
         self.apiKey = apiKey
 
 
-async def main():
+async def sync_function():
     credentials = Credentials("163084b1-5e4c-49c5-b7ec-e41ccca65642",
                               "b6b33cc0-8fe4-4829-bf27-2df41d3f74a9",
                               "yBHhI6yW9vYG+4bi4VwanQVvyk6UYuDtWcZSn1oHT9Q=")
@@ -58,5 +59,32 @@ async def main():
     print(await search.count_answered_done_requests())
 
 
+async def async_function():
+    credentials = Credentials("163084b1-5e4c-49c5-b7ec-e41ccca65642",
+                              "b6b33cc0-8fe4-4829-bf27-2df41d3f74a9",
+                              "yBHhI6yW9vYG+4bi4VwanQVvyk6UYuDtWcZSn1oHT9Q=")
+    file_instance = KaiStudio(credentials).file_instance()
+    manage_instance = KaiStudio(credentials).manage_instance()
+    search = KaiStudio(credentials).search()
+    files = {"files": open("files/kai-studio v1.1.pdf", "rb")}
+
+    tasks = [file_instance.upload_files(files), file_instance.list_files(),
+             file_instance.delete_files("kai-studio v1.1.pdf"), manage_instance.get_global_health(),
+             manage_instance.is_api_alive(), search.query("what is the history of France TV?", "userid"),
+             search.query("France TV", "userid"),
+             search.get_doc_signature("Azure Blob Storage::b6b33cc0-8fe4-4829-bf27-2df41d3f74a9::Contacter "
+                                      "FranceTV.docx"),
+             search.get_doc_ids(["Azure Blob Storage::b6b33cc0-8fe4-4829-bf27-2df41d3f74a9::Contacter FranceTV.docx",
+                                 "Azure Blob Storage::b6b33cc0-8fe4-4829-bf27-2df41d3f74a9::Histoire FTV.docx"]),
+             search.count_done_requests(), search.count_answered_done_requests()]
+    result_list = await asyncio.gather(*tasks, return_exceptions=False)
+    print(result_list)
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    start_time = time.time()
+    asyncio.run(async_function())
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    start_time = time.time()
+    asyncio.run(sync_function())
+    print("--- %s seconds ---" % (time.time() - start_time))

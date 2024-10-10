40bda0e4ff4440cfd213ac8f3d46b159de3f8f19
RunzhongK-AI <rmei@k-ai.ai>
2024-10-10 20:05:34 +0200

    update

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="kai_sdk_python",
+    version="20241010",
+    author="KAI",
+    author_email="support@wats.ai",
+    description="sdk KAI python",
+    packages=setuptools.find_packages(),
+    install_requires=['httpx==0.27.2', ],
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    url='https://github.com/k-ai-Documentation/sdk-python'
+)
